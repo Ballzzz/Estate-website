a86ae2be0bf5421553a4a49fb98a37d30da224d8
Ballzzz <79385639+Ballzzz@users.noreply.github.com>
2024-11-25 15:51:35 +0700

    Update ProductTable.js

--- fontend/src/components/admin/products/ProductTable.js
+++ fontend/src/components/admin/products/ProductTable.js
@@ -124,6 +124,7 @@ const AllProduct = (props) => {
 };
 
 /* Single Product Component */
+/* Single Product Component */
 const ProductTable = ({ product, deleteProduct, editProduct }) => {
   return (
     <Fragment>
@@ -155,7 +156,7 @@ const ProductTable = ({ product, deleteProduct, editProduct }) => {
           )}
         </td>
         <td className="p-2 text-center">{product.pQuantity}</td>
-        <td className="p-2 text-center">{product.pCategory.cName}</td>
+        <td className="p-2 text-center">{product.pCategory?.cName||"N/A"}</td>
         <td className="p-2 text-center">{product.pOffer}</td>
         <td className="p-2 text-center">
           {moment(product.createdAt).format("lll")}
